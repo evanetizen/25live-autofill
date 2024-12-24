7348ce3d06f5c3503fc4d0c09c397b19dfea0811
Evan X <64918172+evanetizen@users.noreply.github.com>
2024-12-24 01:44:57 -0500

    working until description

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import puppeteer from "puppeteer-core";
+
 (async () => {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({
@@ -49,44 +50,40 @@ import puppeteer from "puppeteer-core";
     editableBody.innerHTML =
       "Virginia Wushu Club practices for competition and performances.";
   });
+})();
+
+/*
 
   await page.locator('input[aria-label="datetime input"]').click();
   await page.locator(`.qtip-content [aria-label^='\"2024-12-26']`).click();
 
-  await page
-    .locator('input[aria-label="Start Time"]')
-    .click({ count: 3 })
-    .fill("8:00 pm");
-  await page
-    .locator('input[aria-label="End Time]')
-    .click({ count: 3 })
-    .fill("10:00 pm");
+  await page.locator('input[aria-label="Start Time"]').fill("8:00 pm");
+  await page.locator('input[aria-label="End Time]').fill("10:00 pm");
+await page.locator(".patternButton").click();
+await page
+.locator('select:has(option[label="Weekly"])')
+.select('select:has(option[label="Weekly"][value="2")', "2");
 
-  await page.locator(".patternButton").click();
-  await page
-    .locator('select:has(option[label="Weekly"])')
-    .select('select:has(option[label="Weekly"][value="2")', "2");
+page
+  .locator('[data-label-id="Tue"] input')
+  .filter((checkbox) => checkbox.classList.contains("ng-empty"))
+  .click();
 
-  page
-    .locator('[data-label-id="Tue"] input')
-    .filter((checkbox) => checkbox.classList.contains("ng-empty"))
-    .click();
+page
+  .locator('[data-label-id="Thu"] input')
+  .filter((checkbox) => checkbox.classList.contains("ng-empty"))
+  .click();
 
-  page
-    .locator('[data-label-id="Thu"] input')
-    .filter((checkbox) => checkbox.classList.contains("ng-empty"))
-    .click();
+await page.locator('.modal-open [aria-label="datetime input"]').click();
 
-  await page.locator('.modal-open [aria-label="datetime input"]').click();
+let selectedMonth = await page
+  .locator(".modal-open .qtip-content .h-col-title")
+  .map((titleDiv) => titleDiv.textContent)
+  .wait();
 
-  let selectedMonth = await page
-    .locator(".modal-open .qtip-content .h-col-title")
-    .map((titleDiv) => titleDiv.textContent)
-    .wait();
+console.log(selectedMonth);
 
-  console.log(selectedMonth);
-
-  await page
-    .locator(`.modal-open .qtip-content [aria-label^='\"2024-12-26']`)
-    .click();
-})();
+await page
+.locator(`.modal-open .qtip-content [aria-label^='\"2024-12-26']`)
+.click();
+*/
